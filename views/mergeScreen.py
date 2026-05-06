aaf88a9dfd0c31ecbba29563930a622ecd63d041
Victor G. Menezes <victormrdv2@gmail.com>
2026-05-06 11:13:18 -0300

    feat: restructure merge screen layout and add button functionalities

--- views/mergeScreen.py
+++ views/mergeScreen.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import (
-    QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog, QStackedWidget
+    QWidget, QVBoxLayout, QPushButton, QLabel, QStackedWidget, QFrame, QHBoxLayout,
+    QGridLayout
 )
 from PySide6.QtCore import Qt
 
@@ -22,15 +23,48 @@ class MergeScreen(QWidget):
 
         # setting and complete process screen
         self.settingStep = QWidget()
-        settingLayout = QVBoxLayout(self.settingStep)
+        mainSettingLayout = QHBoxLayout(self.settingStep)
 
-        self.sttgLabel = QLabel("Merge")
+        # main content
+        mainSide = QWidget()
+        mainSideLayout = QVBoxLayout(mainSide)
+
+        # helper buttons
+        topButtons = QHBoxLayout()
+
         self.backBtn = QPushButton('Back')
+        self.addFilesBtn = QPushButton('+')
 
-        settingLayout.addWidget(self.sttgLabel)
-        settingLayout.addWidget(self.backBtn)
+        topButtons.addWidget(self.backBtn)
+        topButtons.addWidget(self.addFilesBtn)
 
-        self.backBtn.clicked.connect(self.backChoosingFile)
+        mainSideLayout.addLayout(topButtons)
+
+        # grid with the file icons
+        self.fileFrame = QFrame()
+        self.fileFrame.setFrameStyle(QFrame.StyledPanel)
+
+        self.fileLayout = QGridLayout(self.fileFrame)
+
+        mainSideLayout.addWidget(self.fileFrame)
+
+        # Settings side
+        settingSide = QWidget()
+        settingLayout = QVBoxLayout(settingSide)
+
+        self.mergeBtn = QPushButton("Merge")
+        self.mergeBtn.setMinimumHeight(60)
+        
+        nome = QLabel('teste de que esta indo para o lugar correto')
+
+        settingLayout.addWidget(nome)
+        settingLayout.addWidget(self.mergeBtn)
+        settingLayout.addStretch()
+
+
+        # add to main layout
+        mainSettingLayout.addWidget(mainSide, 3)      # Bigger
+        mainSettingLayout.addWidget(settingSide, 1)   # Smaller
 
         # add screen to stack
         self.innerStack.addWidget(self.selectFileStep)
@@ -38,6 +72,9 @@ class MergeScreen(QWidget):
 
         # save the selected files on this var
         self.selectedFile = []
+
+        # buttons commands
+        self.backBtn.clicked.connect(self.backChoosingFile)
 
     def continueToMergeScreen(self, filepaths):
         self.selectedFile = filepaths
